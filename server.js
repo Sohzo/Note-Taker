fd0733687b2074bdb5120b35b3e37ed82d437c81
Jonathan Winter <jonpw2004@outlook.com>
2023-05-09 20:55:21 -0600

    Created route for homepage

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const path = require('path');
+const api = require('./routes/index');
+
+const PORT = process.env.port || 3001;
+
+const app = express();
+
+app.use('/api', api);
+app.use(express.static('public'))
+
+app.get('/', (req,res) =>
+    res.sendFile(path.join(__dirname, '/public/index.html'))
+);
+
+app.listen(PORT, () => 
+    console.log(`http://localhost:${PORT}`)
+)
